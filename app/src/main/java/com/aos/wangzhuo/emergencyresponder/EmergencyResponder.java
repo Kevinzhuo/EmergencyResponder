426bbe4d89ff5e4cc1323142dbdc8750732a2b97
Kevin <kevinwang@ideaculture.com>
2015-08-14 16:42:07 +0800

    EmergencyResponder commit by Kevin 2015/08/14 16:42

--- app/src/main/java/com/aos/wangzhuo/emergencyresponder/EmergencyResponder.java
+++ app/src/main/java/com/aos/wangzhuo/emergencyresponder/EmergencyResponder.java
@@ -4,9 +4,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,8 +21,10 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
 
-import java.lang.reflect.Array;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.locks.ReentrantLock;
 
 
@@ -26,6 +34,8 @@ public class EmergencyResponder extends ActionBarActivity {
     CheckBox locationCheckBox;
     ArrayList<String> requesters;
     ArrayAdapter<String> aa;
+
+    public static final String SENT_SMS = "com.aos.wangzhuo.emergencyresponder.SMS_SENT";
 
     public static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
 
@@ -73,7 +83,67 @@ public class EmergencyResponder extends ActionBarActivity {
     }
 
     public void respond(boolean ok, boolean includeLocation) {
+        String okString = getString(R.string.allClearText);
+        String notOkString = getString(R.string.maydayTest);
+        String outString = ok ? okString : notOkString;
 
+        ArrayList<String> requestersCopy = (ArrayList<String>) requesters.clone();
+
+        for (String to : requestersCopy)
+            respond(to, outString, includeLocation);
+    }
+
+    private void respond(String to, String response, boolean includeLocation) {
+        lock.lock();
+        requesters.remove(to);
+        aa.notifyDataSetChanged();
+        lock.unlock();
+
+        SmsManager sms = SmsManager.getDefault();
+
+        //发送消息
+        sms.sendTextMessage(to, null, response, null, null);
+
+        StringBuilder sb = new StringBuilder();
+
+        //找到当前位置，并且如有需要，将它作为SMS消息发送出去
+        if (includeLocation) {
+            String ls = Context.LOCATION_SERVICE;
+            LocationManager lm = (LocationManager) getSystemService(ls);
+            Location l = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            if (l == null)
+                sb.append("Location unknown.");
+            else {
+                sb.append("I'm @:\n");
+                sb.append(l.toString() + "\n");
+
+                List<Address> addresses;
+                Geocoder g = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    addresses = g.getFromLocation(l.getLatitude(), l.getLongitude(), 1);
+
+                    if (addresses != null) {
+                        Address currentAddress = addresses.get(0);
+                        if (currentAddress.getMaxAddressLineIndex() > 0) {
+                            for (int i = 0; i < currentAddress.getMaxAddressLineIndex(); i++) {
+                                sb.append(currentAddress.getAddressLine(i));
+                                sb.append("\n");
+                            }
+                        } else {
+                            if (currentAddress.getPostalCode() != null)
+                                sb.append(currentAddress.getPostalCode());
+                        }
+                    }
+                } catch (IOException e) {
+                    Log.e("SMS_RESPONDER", "IO Exception.", e);
+                }
+
+                ArrayList<String> locationMsgs = sms.divideMessage(sb.toString());
+                for (String locationMsg : locationMsgs)
+                    sms.sendTextMessage(to, null, locationMsg, null, null);
+            }
+        }
     }
 
     private void startAutoResponder() {
