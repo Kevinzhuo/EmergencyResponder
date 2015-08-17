93523f1cf50a599125f9983ce1bcaea89c20083d
Kevin <kevinwang@ideaculture.com>
2015-08-17 18:22:34 +0800

    EmergencyResponder commit by Kevin 2015/08/17 18:22

---
+++ app/src/main/java/com/aos/wangzhuo/emergencyresponder/AutoResponder.java
@@ -0,0 +1,14 @@
+package com.aos.wangzhuo.emergencyresponder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Kevin Wang on 2015/8/17.
+ */
+public class AutoResponder extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
